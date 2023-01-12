e98cda1afb8515fd457598b77d876b32d142adeb
erictorres0123 <erictorres0123@gmail.com>
2023-01-12 10:26:23 -0600

    updated our users tables

--- db/index.js
+++ db/index.js
@@ -5,32 +5,62 @@ const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 async function getAllUsers(){
     const {rows} = await client.query(
-        `SELECT id, username
+        `SELECT id, name, location, active, username
         FROM users;`
     );
 
     return rows;
 }
 
-async function createUser({username, password})
+async function createUser({
+    username,
+    password,
+    name,
+    location})
 {
     try
     {
-        const result = await client.query(`
-            INSERT INTO users(username, password) VALUES ($1, $2)
+        const {rows: [user] } = await client.query(`
+            INSERT INTO users(username, password, name, location) 
+            VALUES ($1, $2, $3, $4)
             ON CONFLICT (username) DO NOTHING
-            RETURNING id, username;
-        `,[username, password]);
-        return result;
+            RETURNING *;
+        `,[username, password, name, location]);
+        return user;
     }catch(error)
     {
         throw error;
     }
 }
 
+async function updateUser(id, fields = {}) {
+    // build the set string
+    const setString = Object.keys(fields).map(
+      (key, index) => `"${ key }"=$${ index + 1 }`
+    ).join(', ');
+  
+    // return early if this is called without fields
+    if (setString.length === 0) {
+      return;
+    }
+  
+    try {
+      const { rows: [ user ] } = await client.query(`
+        UPDATE users
+        SET ${ setString }
+        WHERE id=${ id }
+        RETURNING *;
+      `, Object.values(fields));
+  
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
 
 module.exports = {
     client,
     getAllUsers,
     createUser,
+    updateUser,
 }
--- db/seed.js
+++ db/seed.js
@@ -1,14 +1,22 @@
 //grab our client with destructuring from the export in index.js
-const {client, getAllUsers, createUser} = require('./index');
+const {client, getAllUsers, createUser, updateUser} = require('./index');
 
 async function testDB() {
     try{
         console.log('Starting to test database...');
         
         //queries are promises, so we can await them
+        console.log("Calling getAllUsers")
         const users = await getAllUsers();
-        console.log('getAllUsers: ', users);
-
+        console.log("Result:", users);
+    
+        console.log("Calling updateUser on users[0]")
+        const updateUserResult = await updateUser(users[0].id, {
+          name: "Newname Sogood",
+          location: "Lesterville, KY"
+        });
+        console.log("Result:", updateUserResult);
+    
         
         console.log('Finished database tests!');
 
@@ -41,7 +49,10 @@ async function createTables(){
         CREATE TABLE users (
             id  SERIAL PRIMARY KEY,
             username varchar(255) UNIQUE NOT NULL,
-            password varchar(255) NOT NULL
+            password varchar(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            location VARCHAR(255) NOT NULL,
+            active BOOLEAN DEFAULT true
         );
         `);
         console.log('Finished building tables!')
@@ -52,14 +63,18 @@ async function createTables(){
 }
 
 //function to attempt to create users
+
+
+
 async function createInitialUsers()
 {
     try
     {
         console.log("Starting to create users...");
-        const albert = await createUser({username: 'albert', password: 'bertie99'});
-        const sansdra = await createUser({username: 'sandra', password: '2sandy4me'});
-        const glamgal = await createUser({username: 'glamgal', password: 'soglam'});
+
+        const albert = await createUser({username: 'albert', password: 'bertie99', name:'al', location:'IL',});
+        const sansdra = await createUser({username: 'sandra', password: '2sandy4me', name:'sandy', location:'WI',});
+        const glamgal = await createUser({username: 'glamgal', password: 'soglam', name:'glamy', location:'NY',});
 
         //console.log(albert);
     }catch(error)
