3ab5c400db9005b4ac321521b414017328e64648
An Nguyen <annguyen@Ans-MacBook-Pro.lan>
2023-01-19 10:47:39 -0600

    update

--- api/index.js
+++ api/index.js
@@ -6,4 +6,7 @@ const apiRouter = express.Router();
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
 
+const postsRouter = require('./posts');
+apiRouter.use('/posts', postsRouter);
+
 module.exports = apiRouter;
--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,8 @@
+
 const express = require('express');
 const postsRouter = express.Router();
+const { getAllPosts } = require('../db');
+
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -7,9 +10,12 @@ postsRouter.use((req, res, next) => {
   next(); 
 });
 
-postsRouter.get('/', (req, res) => {
+postsRouter.get('/', async(req, res) => {
+    const posts = await getAllPosts();
+    console.log(posts)
+
   res.send({
-    posts: []
+    posts
   });
 });
 
