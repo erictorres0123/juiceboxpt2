90ceabb0cbbc695b19a6a4359907e2f16c3a8d20
An Nguyen <annguyen@Ans-MacBook-Pro.lan>
2023-01-20 15:57:10 -0600

    update

--- api/posts.js
+++ api/posts.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const postsRouter = express.Router();
 const { getAllPosts } = require('../db');
+const { requireUser } = require('./utils');
 
 
 postsRouter.use((req, res, next) => {
@@ -19,4 +20,26 @@ postsRouter.get('/', async(req, res) => {
   });
 });
 
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {authorId, title, content};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    res.send({post});
+    // otherwise, next an appropriate error object 
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 module.exports = postsRouter;
